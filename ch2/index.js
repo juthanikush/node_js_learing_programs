bcf454719f895e39c8ef11dc4c6743a600b43f3f
KUSH  JUTHNAI <81456337+juthanikush@users.noreply.github.com>
2022-03-24 19:53:06 +0530

    program-2

---
+++ ch2/index.js
@@ -0,0 +1,10 @@
+/**modul wrapper
+ * (function (exports,require,module,__filename,__dirname){
+ *  module code is actually live here
+ * });
+ */
+//console.log(__filename);
+//console.log(__dirname);
+//console.log(module);
+//console.log(exports);
+console.log(require);
